c57629e8598489937314d930b2c07b3acf41fe90
CedricBm <beaumont.cedric@gmail.com>
2017-08-19 17:34:37 +0200

    Solves the first unit test

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,85 @@
 class Event < ActiveRecord::Base
+  SLOT_DURATION = 30.minutes
+  DAYS_IN_WEEK = 7
+
+  class Availability
+    attr_accessor :date, :slots
+
+    def initialize(date, events_by_kind)
+      @date = date
+      @slots = compute_slots(events_by_kind)
+    end
+
+    def to_hash
+      {date: @date, slots: @slots}
+    end
+
+    private
+
+      def compute_slots(events_by_kind)
+        booked_slots = extract_slots_of_events(events_by_kind["appointment"])
+        open_slots = extract_slots_of_events(events_by_kind["opening"])
+
+        open_slots - booked_slots
+      end
+
+      def extract_slots_of_events(events)
+        slots = []
+
+        events.each do |event|
+          slots += event.extract_slots if event.is_date_corresponding?(@date)
+        end
+
+        slots
+      end
+
+  end
+
+  def self.availabilities(begin_date)
+    availabilities = []
+    events_by_kind = compute_events_by_kind
+
+    # for the next 7 days beginning at the begin_date
+    (0..6).each do |offset|
+      availabilities << Availability.new(begin_date.to_date + offset.days, events_by_kind)
+    end
+
+    availabilities.map(&:to_hash)
+  end
+
+  # I'll assume that an event starts and ends the same day
+  def is_date_corresponding?(date)
+    if self.starts_at.to_date == date
+      true
+    elsif self.kind == "opening" && self.weekly_recurring
+      days_count_between_dates = self.starts_at.to_date.upto(date).count
+      days_count_between_dates % DAYS_IN_WEEK == 1 # the recurring event reoccurs at the same day
+    else
+      false
+    end
+  end
+
+  def extract_slots
+    slots = []
+    begin_time = self.starts_at
+
+    while begin_time < self.ends_at do
+      slots << begin_time.strftime('%H:%M')
+      begin_time += SLOT_DURATION
+    end
+
+    slots
+  end
+
+  private
+
+    # We could fetch only the relevant events that match the following assertions :
+    # 1) if an event is an opening and it is weekly recurrent.
+    # 2) if an event is an opening and it is not weekly recurrent but the slots are in the week period requested.
+    # 3) if an event is an appointment and the slots are in the week period requested.
+    # But that would be overengineering and would be necessary only if there are a lot of data.
+    def self.compute_events_by_kind
+      Event.order(starts_at: :asc).group_by(&:kind)
+    end
+
 end
